7c28eb4699c627f3dfe54ea15f839e1236eb6dfb
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2018-02-22 13:55:42 +0100

    spitest: Allow other devices on the command line.

--- src/spitest.c
+++ src/spitest.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ftdispi.h"
+
+void usage(char * name)
+{
+    fprintf(stderr, "usage: %s [options]\n", name);
+    fprintf(stderr, "\t-p <number> Search for device with PID == number\n");
+    fprintf(stderr, "\t-v <number> Search for device with VID == number\n");
+}
 
 int main(int argc, char **argv)
 {
     struct ftdi_context    fc;
     struct ftdispi_context fsc;
+    int vid = 0x0403;
+    int pid = 0x6011;
+
     int i;
 
     if (ftdi_init(&fc) < 0)
@@ -12,7 +23,22 @@ int main(int argc, char **argv)
         fprintf(stderr, "ftdi_init failed\n");
         return 1;
     }
-    i = ftdi_usb_open(&fc, 0x0403, 0x6011);
+    while ((i = getopt(argc, argv, "p:v:")) != -1)
+    {
+        switch (i)
+        {
+        case 'p':
+            pid = strtoul(optarg, NULL, 0);
+            break;
+        case 'v':
+            vid = strtoul(optarg, NULL, 0);
+            break;
+        default:
+            usage(argv[0]);
+            exit(-1);
+        }
+    }
+    i = ftdi_usb_open(&fc, vid, pid);
     if (i < 0 && i != -5)
     {
         fprintf(stderr,
@@ -20,10 +46,10 @@ int main(int argc, char **argv)
                 ftdi_get_error_string(&fc));
         exit(-1);
     }
-    ftdispi_open(&fsc, &fc, INTERFACE_A);
-    ftdispi_setmode(&fsc, 1, 0, 0, 0, 0, 0);
+    ftdispi_open(&fsc, &fc, INTERFACE_ANY);
+    ftdispi_setmode(&fsc, 1, 0, 0, 0, 0, FTDISPI_GPO0);
     ftdispi_setclock(&fsc, 200000);
-    ftdispi_setloopback(&fsc, 0);
+//    ftdispi_setloopback(&fsc, 0);
     ftdispi_write(&fsc, "Test", 4, 0);
     ftdispi_close(&fsc, 1);
     return 0;
